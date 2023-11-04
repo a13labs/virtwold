a7a87f36356319e2acbddaa426e016a68a2e3ae1
Alexandre Pires <c.alexandre.pires@gmail.com>
2023-11-04 14:59:43 +0100

    Major changes - Changed BPF filter - Read MAC from LinkLayer - Added support for override libvirt connection - Added github action - Minor improvements

--- virtwold.go
+++ virtwold.go
@@ -12,9 +12,7 @@
 package main
 
 import (
-	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"net"
 	"strings"
@@ -26,12 +24,17 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
+var iface string           // Interface we'll listen on
+var libvirtAddrType string // Interface we'll listen on
+var libvirtAddr string     // Interface we'll listen on
+
 func main() {
-	var iface string                                            // Interface we'll listen on
-	var buffer = int32(1600)                                    // Buffer for packets received
-	var filter = "udp and broadcast and (len = 102 or len = 144 or len=234)" // PCAP filter to catch UDP WOL packets
+	var buffer = int32(1600)                        // Buffer for packets received
+	var filter = "ether proto 0x0842 or udp port 9" // PCAP filter to catch UDP WOL packets
 
 	flag.StringVar(&iface, "interface", "", "Network interface name to listen on")
+	flag.StringVar(&libvirtAddrType, "socket", "unix", "Libvirt socket type")
+	flag.StringVar(&libvirtAddr, "addr", "/var/run/libvirt/libvirt-sock", "Libvirt address")
 	flag.Parse()
 
 	if !deviceExists(iface) {
@@ -40,10 +43,11 @@ func main() {
 
 	handler, err := pcap.OpenLive(iface, buffer, false, pcap.BlockForever)
 	if err != nil {
-		log.Fatalf("failed to open device: %v", err)
+		log.Fatalf("Failed to open device: %v", err)
 	}
 	defer handler.Close()
 
+	log.Printf("Using device: %s", iface)
 	if err := handler.SetBPFFilter(filter); err != nil {
 		log.Fatalf("Something in the BPF went wrong!: %v", err)
 	}
@@ -52,48 +56,39 @@ func main() {
 	source := gopacket.NewPacketSource(handler, handler.LinkType())
 	for packet := range source.Packets() {
 		// Called for each packet received
-		fmt.Printf("Received WOL packet, ")
-		mac, err := GrabMACAddr(packet)
-		if err != nil {
-			log.Fatalf("Error with packet: %v", err)
+		link := packet.LinkLayer()
+		mac := link.LinkFlow().Dst().String()
+		log.Printf("Received WOL packet.\n")
+		if WakeVirtualMachine(mac) {
+			log.Printf("Matched VM with MAC address on this host.\n")
+		} else {
+			log.Printf("No VM with matched MAC address found on this host.\n")
 		}
-		WakeVirtualMachine(mac)
 	}
-}
-
-// Return the first MAC address seen in the WOL packet
-func GrabMACAddr(packet gopacket.Packet) (string, error) {
-	app := packet.ApplicationLayer()
-	if app != nil {
-		payload := app.Payload()
-		mac := fmt.Sprintf("%02x:%02x:%02x:%02x:%02x:%02x", payload[12], payload[13], payload[14], payload[15], payload[16], payload[17])
-		fmt.Printf("found MAC: %s\n", mac)
-		return mac, nil
-	}
-	return "", errors.New("no MAC found in packet")
 }
 
 func WakeVirtualMachine(mac string) bool {
 	// Connect to the local libvirt socket
-	c, err := net.DialTimeout("unix", "/var/run/libvirt/libvirt-sock", 2*time.Second)
+	c, err := net.DialTimeout(libvirtAddrType, libvirtAddr, 2*time.Second)
 	if err != nil {
-		log.Fatalf("failed to dial libvirt: %v", err)
+		log.Fatalf("Failed to dial libvirt: %v", err)
 	}
 
 	l := libvirt.New(c)
 	if err := l.Connect(); err != nil {
-		log.Fatalf("failed to connect: %v", err)
+		log.Fatalf("Failed to connect: %v", err)
 	}
 
 	// Get a list of all VMs (aka Domains) configured so we can loop through them
 	flags := libvirt.ConnectListDomainsActive | libvirt.ConnectListDomainsInactive
 	domains, _, err := l.ConnectListAllDomains(1, flags)
 	if err != nil {
-		log.Fatalf("failed to retrieve domains: %v", err)
+		log.Fatalf("Failed to retrieve domains: %v", err)
 	}
 
+	macFound := false
 	for _, d := range domains {
-		//fmt.Printf("%d\t%s\t%x\n", d.ID, d.Name, d.UUID)
+		//log.Printf("%d\t%s\t%x\n", d.ID, d.Name, d.UUID)
 
 		// Now we get the XML Description for each domain
 		xmldesc, err := l.DomainGetXMLDesc(d, 0)
@@ -111,48 +106,51 @@ func WakeVirtualMachine(mac string) bool {
 		for _, list := range xmlquery.Find(querydoc, "//domain/devices/interface/mac/@address") {
 			// Use the strings.EqualFold function to do a case-insensitive comparison of MACs
 			if strings.EqualFold(list.InnerText(), mac) {
+				macFound = true
 				stateInt, _, err := l.DomainGetState(d, 0)
 				if err != nil {
-					log.Fatalf("failed to check domain state: %v", err)
+					log.Fatalf("Failed to check domain state: %v", err)
 				}
 
 				state := libvirt.DomainState(stateInt)
-				// fmt.Printf("Domain state is %v\n", state)
+				// log.Printf("Domain state is %v\n", state)
 
 				switch state {
 				case libvirt.DomainShutoff, libvirt.DomainCrashed:
-					fmt.Printf("Waking system: %s at MAC %s\n", d.Name, mac)
+					log.Printf("Waking system: %s at MAC %s\n", d.Name, mac)
 					if err := l.DomainCreate(d); err != nil {
 						log.Fatalf("Failed to start domain: %v", err)
 					}
 				case libvirt.DomainPmsuspended:
-					fmt.Printf("PM Wakeup system: %s at MAC %s\n", d.Name, mac)
+					log.Printf("PM Wakeup system: %s at MAC %s\n", d.Name, mac)
 					if err := l.DomainPmWakeup(d, 0); err != nil {
 						log.Fatalf("Failed to pm wakeup domain: %v", err)
 					}
 				case libvirt.DomainPaused:
-					fmt.Printf("Resume system: %s at MAC %s\n", d.Name, mac)
+					log.Printf("Resume system: %s at MAC %s\n", d.Name, mac)
 					if err := l.DomainResume(d); err != nil {
 						log.Fatalf("Failed to resume domain: %v", err)
 					}
-			        default:
-					fmt.Printf("System %s is already running or in a state that cannot be woken from. State: %d\n", d.Name, state)
+				default:
+					log.Printf("System %s is already running or in a state that cannot be woken from. State: %d\n", d.Name, state)
 				}
+
+				break
 			}
 		}
 	}
 
 	if err := l.Disconnect(); err != nil {
-		log.Fatalf("failed to disconnect: %v", err)
+		log.Fatalf("Failed to disconnect: %v", err)
 	}
 
-	return true
+	return macFound
 }
 
 // Check if the network device exists
 func deviceExists(interfacename string) bool {
 	if interfacename == "" {
-		fmt.Printf("No interface to listen on specified\n\n")
+		log.Printf("No interface to listen on specified\n\n")
 		flag.PrintDefaults()
 		return false
 	}
